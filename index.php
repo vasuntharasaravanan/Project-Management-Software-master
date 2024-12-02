2c8e4d94b2b1692b917f56a5e5a5f796a683a0c6
vasuntharasaravanan <vasuntharavasunthara93@gmail.com>
2024-12-02 11:46:34 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * @author Kiril Kirkov
+ * https://github.com/issue-tracking-system/Project-Management-Software
+ */
+
+session_start();
+define('APPLICATION_LOADED', true);
+define('HAS_SUBDOMAIN_SUPPORT', false);
+
+if(!HAS_SUBDOMAIN_SUPPORT) {
+    define('ACCOUNT_DOMAIN', 'global');
+    define('COMPANY_NAME', 'Issue Tracking System');
+}
+require_once 'inc/db.php';
+require_once 'classes/class.main.php';
+if(HAS_SUBDOMAIN_SUPPORT) {
+    require_once 'classes/class.subdomain.php';
+} else {
+    define('ACCOUNT_ID', '1');
+}
+
+if (DEBUG_MODE === false) {
+    error_reporting(E_ALL ^ E_NOTICE | E_WARNING);
+}
+
+try {
+    
+    if (!isset($_GET['page'])) {
+        $_GET['page'] = 'login';
+    }
+    $main = new Main();
+    $main->run($_GET['page'], !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+
+} catch (Exception $e) {
+    if (DEBUG_MODE === true) {
+        echo 'New Exception: ' . $e->getMessage();
+    } else {
+        writeLog('Time: ' . date("Y.m.d H.m.s", time()) . "\nDomain:" . $_SERVER['HTTP_HOST'] . "\nNew Exception:" . $e->getMessage() . "\n\n");
+        include '_html_parts/exeption_page.php';
+    }
+}
